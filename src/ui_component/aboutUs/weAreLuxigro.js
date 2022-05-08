2ee60ebcb5ee3eaee29af778f370d86c7b3c7522
Balram Yadav <balramyadav@Balrams-MacBook-Air.local>
2022-05-08 19:16:46 +0530

    image fix

--- src/ui_component/aboutUs/weAreLuxigro.js
+++ src/ui_component/aboutUs/weAreLuxigro.js
@@ -8,7 +8,7 @@ export default function WeAreLuxigro() {
 
 
         <div className="about_box">
-            <img src="/images/luxigrologo.svg" alt="" className="luxigro_logo" />
+            <img src="luxigrologo.svg" alt="" className="luxigro_logo" />
         </div>
             <div className="about_us_text">
             Over the last 20 years, we’ve earned a reputation as a specialist construction bussiness in all over india. Our goal is build better and more sustainable future  
